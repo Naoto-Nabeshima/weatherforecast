ee93391ba1f80c145310cd0e594308ed4c87d116
yuka-t <ndf25.882@gmail.com>
2016-11-15 01:53:07 +0900

    CreateButton

--- WeatherForecast/app/src/main/java/com/org/app/weatherforecast/WeatherForecastMainActivity.java
+++ WeatherForecast/app/src/main/java/com/org/app/weatherforecast/WeatherForecastMainActivity.java
@@ -2,12 +2,25 @@ package com.org.app.weatherforecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WeatherForecastMainActivity extends AppCompatActivity {
+
+    private Button mUpdateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast_main);
+
+        mUpdateButton = (android.widget.Button)findViewById(R.id.update_button);
+        mUpdateButton.setOnClickListener(UpdateListner);
     }
+
+    private View.OnClickListener UpdateListner = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        }
+    };
 }
